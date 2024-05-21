3ca080f102b3383fd90d60bcc15cadec1b5ee5a0
Manuela Schneider <manuelagottschalck1103@gmail.com>
2024-05-20 21:57:55 -0300

    Adicionando as entradas de todas as variáveis

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,11 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+
         int numero;
-        String agencia, nomeCliente;
+        String agencia, nome;
         float saldo;
 
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Por favor, insira o número da agência:");
+        agencia = scanner.next();
 
+        System.out.println("Por favor, insira o número");
+        numero = scanner.nextInt();
+
+        scanner.nextLine(); ///adionado pois a classe scanner le uma nova linha como um caracter, com a classe adiconada neste ponto do codigo, ele lera o caracetr"\n"
+    
+
+        System.out.println("Por favor, digite seu nome completo");
+        nome = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo de sua conta");
+        saldo = scanner.nextFloat();
         
     }
 }
